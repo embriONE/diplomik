322b90f1bf408fbb1ceb180a430ab411fb7bec35
Даня <@DANILICH>
2024-05-17 11:00:54 +0300

    Добавьте файлы проекта.

---
+++ src/Board/Contracts/Board.Contracts/Account/AccountDto.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Claims;
+
+namespace Board.Contracts.Account
+{
+    /// <summary>
+    /// Информация об аккаунте.
+    /// </summary>
+    public class AccountDto
+    {
+        /// <summary>
+        /// Идентификатор.
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Логин.
+        /// </summary>
+        public string Login { get; set; }
+        
+        /// <summary>
+        /// Электронный адрес.
+        /// </summary>
+        public string Email { get; set; }
+
+        /// <summary>
+        /// Признак блокировки.
+        /// </summary>
+        public bool IsBlocked { get; set; }
+
+
+        public bool IsAuthenticated { get; set; }
+        public string Scheme { get; set; }
+        public List<Claim> Claims { get; set; } = new List<Claim>();
+
+    }
+}
